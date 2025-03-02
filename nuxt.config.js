602bfd3ef8899e8adbaf57f85d0dbfcb762a887b
shouzhong <15270737104@163.com>
2025-03-02 13:29:27 +0800

    2

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   // 为GitHub Pages部署设置静态生成
   ssr: false,
